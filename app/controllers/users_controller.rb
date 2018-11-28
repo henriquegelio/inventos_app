6d2b0e480f7fc379177783d297f784b2d4dea461
EC2 Default User <ec2-user@ip-172-31-8-132.us-east-2.compute.internal>
2018-11-28 08:01:05 +0000

    Initialize Rep

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,74 @@
+class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+  
+  def new
+    @user = User.new
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
+    @microposts = @user.microposts.paginate(page: params[:page])
+  end
+  
+  def index
+    @users = User.where(activated: true).paginate(page: params[:page])
+  end
+  
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
+      # Handle a successful save.
+      #log_in(@user)
+      #flash[:success] = "Welcome to the Sample App!"
+      #redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    #@user = User.find(params[:id])    Not necessary because of correct user and before filter (before action)
+  end
+  
+  def update
+    #@user = User.find(params[:id])    Not necessary because of correct user and before filter (before action)
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    # Before filters
+    
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+end
